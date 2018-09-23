a6007d4832d206e68f7083f8f2e59bac2faeff3d
knts0 <shimmer22.0204@gmail.com>
2018-09-24 01:42:41 +0900

    登録機能の実装

--- app/controllers/TodoController.scala
+++ app/controllers/TodoController.scala
@@ -3,9 +3,14 @@ package controllers
 import javax.inject._
 import play.api.mvc._
 
+import play.api.data._
+import play.api.data.Forms._
+
 import services._
 
 class TodoController @Inject()(todoService: TodoService, mcc: MessagesControllerComponents) extends MessagesAbstractController(mcc) {
+
+  val todoForm: Form[String] = Form("name" -> nonEmptyText)
 
   def helloworld() = Action { implicit request: MessagesRequest[AnyContent] =>
     Ok("Hello World")
@@ -14,6 +19,16 @@ class TodoController @Inject()(todoService: TodoService, mcc: MessagesController
   def list() = Action { implicit request: MessagesRequest[AnyContent] =>
     val items: Seq[Todo] = todoService.list()
     Ok(views.html.list(items))
+  }
+
+  def todoNew = Action { implicit request: MessagesRequest[AnyContent] =>
+    Ok(views.html.createForm(todoForm))
+  }
+
+  def todoAdd() = Action { implicit request: MessagesRequest[AnyContent] =>
+    val name: String = todoForm.bindFromRequest().get
+    todoService.insert(Todo(name))
+    Redirect(routes.TodoController.list())
   }
 
 }
